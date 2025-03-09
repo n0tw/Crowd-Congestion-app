c1d029dc72650a317cb3359d8c8aac440236db3f
n0tw <91394430+n0tw@users.noreply.github.com>
2025-03-09 17:40:08 +0200

    remove Google Apikey from config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const googleCredentials = {
-    apiKey: 'AIzaSyDD7kMR3hUWEaT5fDYqCGxHW1MwuVVUdLc', // Replace with your actual API key
+    apiKey: '', // Replace with your actual API key
     clientId: '260243600706-e5u8mdaiap2q54eo9frj7r40lnjnq2ro.apps.googleusercontent.com', // Replace with your actual Client ID
 };
